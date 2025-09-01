b23305085471eb8fc957fd0c575c7ecaf7008f77
Flyn San <flynsarmy@gmail.com>
2025-09-02 00:47:31 +1000

    Display a File icon for thumbnails that failed to generate

--- addons/asset_placer/ui/components/asset_thumbnail.gd
+++ addons/asset_placer/ui/components/asset_thumbnail.gd
@@ -2,6 +2,8 @@
 extends TextureRect
 class_name AssetThumbnail
 
+const no_preview_tex: CompressedTexture2D = preload("res://addons/asset_placer/ui/components/File.png")
+
 var resource: AssetResource
 var previewer: EditorResourcePreview
 var last_time_modified: int = 0
@@ -15,7 +17,7 @@ func preview_resource(resource: AssetResource) -> void:
 	if Engine.is_editor_hint() and resource and resource.scene:
 		last_time_modified = FileAccess.get_modified_time(resource.scene.resource_path)
 		previewer = EditorInterface.get_resource_previewer()
-		previewer.queue_edited_resource_preview(resource.scene, self, "_on_preview_generated", null)
+		previewer.queue_edited_resource_preview(resource.scene, self, "_on_preview_generated", resource)
 
 func _process(_delta: float) -> void:
 	if Engine.is_editor_hint() and resource and resource.scene:
@@ -23,5 +25,8 @@ func _process(_delta: float) -> void:
 		if new_time_modified != last_time_modified:
 			preview_resource(resource)
 
-func _on_preview_generated(path: String, texture: Texture2D,  thumbnail: Texture2D, data: Variant) -> void:
-	self.texture = texture
+func _on_preview_generated(path: String, generated_texture: Texture2D,  thumbnail: Texture2D, data: AssetResource) -> void:
+	if generated_texture == null:
+		texture = no_preview_tex
+	else:
+		texture = generated_texture
